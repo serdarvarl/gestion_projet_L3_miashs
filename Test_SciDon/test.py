82aee4478ddada9176b465bc505ff2178d15e8ab
Serdar <181284499+serdarvarl@users.noreply.github.com>
2025-10-24 11:12:55 +0200

    create test py
    create fichier test pour python

---
+++ Test_SciDon/test.py
@@ -0,0 +1,8 @@
+"""
+Page module for SciDon tests.
+This module contains functions and classes to facilitate testing of the SciDon application.
+"""
+
+def sum(a, b):
+    """Returns the sum of two numbers."""
+    return a + b;
