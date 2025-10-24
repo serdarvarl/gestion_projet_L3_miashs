162b518e66e8344f6c8442b61dcc9c8d0632bc06
Serdar <181284499+serdarvarl@users.noreply.github.com>
2025-10-24 13:37:07 +0200

    rangement
    rangment

--- Test_SciDon/test.py
+++
@@ -1,8 +0,0 @@
-"""
-Page module for SciDon tests.
-This module contains functions and classes to facilitate testing of the SciDon application.
-"""
-
-def sum(a, b):
-    """Returns the sum of two numbers."""
-    return a + b;
